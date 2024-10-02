ba8d4ef3c6cae81d90ee65c38f33dadedb3cd123
Luthando496 <luthandodidiza197@gmail.com>
2024-10-02 01:25:09 -0700

    changed index name

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -14,17 +14,17 @@ const Categories = ({ currency,restaurantData,exchange }) => {
   return (
     // parent container of each list 
     <div className='flex flex-col gap-3'>
-      {restaurantData.categories.map((category, index) => (
+      {restaurantData.categories.map((category, categoryIndex) => (
         /// each list of categories and items
-    <div key={index} className="lg:w-[482px] md:w-full rounded-lg bg-white  shadow-2xl foods">
-        <ul key={index *6} className="">
+    <div key={categoryIndex} className="lg:w-[482px] md:w-full rounded-lg bg-white  shadow-2xl foods">
+        <ul key={categoryIndex *6} className="">
           <li
-            onClick={() => toggleCategory(index)}
+            onClick={() => toggleCategory(categoryIndex)}
             className="max-w-full flex justify-between items-center cursor-pointer px-4"
           >
             <div className="flex gap-1 items-center py-5 ">
               <span className='fonts-logos'>
-                {openIndex === index ? <MdOutlineKeyboardArrowUp className='fonts-logos'  /> : <RiArrowRightWideFill className='fonts-logos' />}
+                {openIndex === categoryIndex ? <MdOutlineKeyboardArrowUp className='fonts-logos'  /> : <RiArrowRightWideFill className='fonts-logos' />}
               </span>
               <h2 className="lg:text-xl text-base font-light">{category.categoryName}</h2>
             </div>
@@ -33,7 +33,7 @@ const Categories = ({ currency,restaurantData,exchange }) => {
         
 
         {/*  */}
-          {openIndex === index && (
+          {openIndex === categoryIndex && (
             category.items.map((food, index) => (
               <div key={index} className="w-full bg-gray-500/30 py-3">
                 <p className="flex justify-between px-[4rem] py-1 text-black/60">
